eb6f9f5d846ee76542f01364bba1bcf107a325ce
Ruslan Bilalov <47795314+fradaaa@users.noreply.github.com>
2023-05-22 17:14:15 +0800

    Setup Routes

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import About from "./about";
+import Posts from "./posts";
+import User from "./user";
+
+export { About, Posts, User };
---
+++ src/routes/posts.tsx
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <div>
+      <h1>ROOT</h1>
+    </div>
+  );
+};
+
+export default Posts;
